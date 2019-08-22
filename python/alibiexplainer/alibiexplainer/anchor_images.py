6c2de424a0f56df00a2290256ffa6f9895e639aa
giovac73 <giova.physics@gmail.com>
2019-08-22 15:51:52 +0100

    temporary fix for shapes

--- python/alibiexplainer/alibiexplainer/anchor_images.py
+++ python/alibiexplainer/alibiexplainer/anchor_images.py
@@ -4,7 +4,6 @@ import logging
 import joblib
 import alibi
 import numpy as np
-import pandas as pd
 from alibiexplainer.explainer_wrapper import ExplainerWrapper
 
 logging.basicConfig(level=kfserving.server.KFSERVER_LOGLEVEL)
@@ -14,13 +13,13 @@ class AnchorImages(ExplainerWrapper):
 
     def __init__(self, predict_fn: Callable, explainer=Optional[alibi.explainers.AnchorImage], **kwargs):
         self.predict_fn = predict_fn
-        self.anchors_images: Optional[alibi.explainers.AnchorImage] = explainer
-        if self.anchors_images is None:
+        self.anchors_image: Optional[alibi.explainers.AnchorImage] = explainer
+        if self.anchors_image is None:
             self.prepare(**kwargs)
         else:
-            pass
+            # pass
             # self.prepare(**kwargs)
-            # self.input_shape = self.anchors_images.input_shape
+            self.input_shape = self.anchors_image.image_shape
 
     def prepare(self, training_data_url=None, input_shape_url=None, **kwargs):
         print(training_data_url)
@@ -31,30 +30,28 @@ class AnchorImages(ExplainerWrapper):
             training_data = joblib.load(training_data_file)
         else:
             pass
-            # raise Exception("Anchor_images requires training data")
 
         if not input_shape_url is None:
             logging.info("Loading input shape file %s" % input_shape_url)
             input_shape_file = kfserving.Storage.download(input_shape_url)
             self.input_shape = joblib.load(input_shape_file)
         else:
-            pass
-            # raise Exception("Anchor_images requires input shape")
+            raise Exception("Anchor_image requires input shape")
 
         logging.info("Creating AnchorImages")
-        self.anchors_images = alibi.explainers.AnchorImage(predict_fn=self.predict_fn,
-                                                            image_shape=self.input_shape)
+        self.anchors_image = alibi.explainers.AnchorImage(predict_fn=self.predict_fn,
+                                                           image_shape=self.input_shape)
 
     def explain(self, inputs: List) -> Dict:
-        if not self.anchors_images is None:
+        if not self.anchors_image is None:
             arr = np.array(inputs)
             # set anchor_images predict function so it always returns predicted class
             # See anchor_images.__init__
             if np.argmax(self.predict_fn(arr).shape) == 0:
-                self.anchors_images.predict_fn = self.predict_fn
+                self.anchors_image.predict_fn = self.predict_fn
             else:
-                self.anchors_images.predict_fn = lambda x: np.argmax(self.predict_fn(x), axis=1)
-            anchor_exp = self.anchors_images.explain(arr)
+                self.anchors_image.predict_fn = lambda x: np.argmax(self.predict_fn(x), axis=1)
+            anchor_exp = self.anchors_image.explain(arr)
             return anchor_exp
         else:
             raise Exception("Explainer not initialized")
